d00333105d07f28f55986065212365c020d5fc8c
Arewa Olakunle <danceasarxx@gmail.com>
2017-01-29 16:23:53 +0100

    The contrib library does not exist
    The contrib library is the merge of tools.fns and tools.macros. It
    was not in the project.clj file.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.395"]
-                 [com.novemberain/monger "3.1.0"]])
+                 [com.novemberain/monger "3.1.0"]
+                 [contrib "0.1.0"]])
